d4555ac0166ff03b0c23f526c906aa16094cd8da
Sydney Kasongo <sydneybleuops@gmail.com>
2019-05-28 21:09:57 +0200

    Commit All

--- btre/accounts/views.py
+++ btre/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User #It comes default with Django
+from contacts.models import Contact
 
 
 from django.http import HttpResponse
@@ -72,4 +73,10 @@ def logout(request):
     return redirect('index')
 
 def dashboard(request):
-    return render(request,'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context ={
+        'contacts': user_contacts
+    }
+
+    return render(request,'accounts/dashboard.html',context)
